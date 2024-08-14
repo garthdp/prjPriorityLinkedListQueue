332671dd6fa7dff1e9dd7411158ecb698f8b34a9
gartdp <garthdp4@gmail.com>
2024-08-14 13:15:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace prjPriorityLinkedListQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queues QP = new Queues();
+            QPNode qPNode = QP.newNode(4, 1);
+            qPNode = QP.push(qPNode, 5, 2);
+            qPNode = QP.push(qPNode, 6, 3);
+            qPNode = QP.push(qPNode, 7, 4);
+            while (QP.isEmpty(qPNode) == 0)
+            {
+                Console.WriteLine("{0:D} ", QP.peek(qPNode));
+                qPNode = QP.pop(qPNode);
+            }
+        }
+    }
+}
---
+++ QPNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjPriorityLinkedListQueue
+{
+    internal class QPNode
+    {
+        public int data;
+        public int rating;
+        public QPNode next;
+    }
+}
